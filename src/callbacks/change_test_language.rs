d5fbcec82591833480fec6097de48583b2313ebd
ndm <73387944+username1419@users.noreply.github.com>
2026-08-05 11:28:04 +1200

    feat: language change functionality
    doesnt display on commandline

--- src/callbacks/change_test_language.rs
+++ src/callbacks/change_test_language.rs
@@ -1,8 +1,13 @@
-use std::sync::Arc;
+use std::{ffi::OsString, hash::Hash, sync::Arc};
 
 use tokio::sync::Mutex;
 
-use crate::{State, command::Command, typing_test::word_list};
+use crate::{
+    State,
+    command::Command,
+    notify::{QuickNotify, debug, enotify, error, notify},
+    typing_test::word_list,
+};
 
 /// Creates the "Change language" command.
 pub(crate) fn create() -> Command {
@@ -11,9 +16,46 @@ pub(crate) fn create() -> Command {
         "Change language".into(),
         crate::command::CommandGroup::Test,
         async move || Ok(true),
-        None,
+        Some(0),
         async move |s: Arc<Mutex<State>>| {
             s.lock().await.commandline.reset();
+
+            let choices = match word_list::get_word_lists().await {
+                Ok(choices) => choices,
+                Err(e) => {
+                    return Err(error!(e).to_string());
+                }
+            }
+            .into_iter()
+            .map(|language| {
+                let language_string = language
+                    .to_str()
+                    .unwrap_or_else(|| panic!("language {:?} is not valid unicode", language))
+                    .to_string();
+                Command::new(
+                    format!("changeLanguage_{}", language_string),
+                    format!("Change language: {}", language_string),
+                    crate::command::CommandGroup::Test,
+                    async move || Ok(true),
+                    None,
+                    move |_| {
+                        let language = language.clone();
+                        async move {
+                            word_list::update_and_get_words(language)
+                                .await
+                                .map(|_| ())
+                                .map_err(|e| e.to_string())
+                        }
+                    },
+                )
+            })
+            .collect();
+
+            s.lock().await.commandline.prompt_command(
+                "Select language...".into(),
+                choices,
+                move |_input, _command_opt| (),
+            );
 
             Ok(())
         },
